c8141bfabe1da95f0f06795335a0cee0b3b3d56e
AnnaTveritnyeva <atveretniv@gmail.com>
2023-02-04 15:06:21 +0200

    Teacher's solution to exercise 11

--- sets.py
+++ sets.py
@@ -4,9 +4,7 @@ ls2 = [3, 4, 5, 3, 7, 5, 9, 10, 20, 10]
 print(set(ls2))
 print(set(ls1) & set(ls2))
 
-ls3 = ls1
-ls3.remove(20)
-ls3.remove(40)
+ls3 = list(set(ls1) - {20, 40})
 print(ls3)
 
-print(set(ls1)-set(ls2))
+print(set(ls1) - set(ls2))
